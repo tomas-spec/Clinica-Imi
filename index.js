66ac9959350e5bb422c1f8881caf0c13380544f2
Tomas Duran <tomasdurannnnn5@gmail.com>
2024-03-13 22:21:17 -0300

    Arreglando

---
+++ index.js
@@ -0,0 +1,7 @@
+//Udemy
+//Ingresos de usuario
+//Pone un input en el html, 
+//<input type= "text" id= "nombreUsuario">
+//<button onclick= "saludarUsuario()"> Saludar</button>
+// Aca en Javascript hacemos: function saludarUsuario(){
+//                            alert("Hola " + document.getElementById("nombreUsuario").value)
